4dce85044d1dbff620cbfd2275196baf8e923c8b
Alex Posidelov <alex.posidelov@gmail.com>
2024-11-25 17:06:38 +0200

    add PROJECT_PATH env variable

---
+++ docroot/modules/custom/rpx_stripe/src/Form/UserStripeTransactionsForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\rpx_stripe\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class UserStripeSettingsForm extends FormBase {
+
+  /**
+   * @inheritDoc
+   */
+  public function getFormId()   {
+
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+}
